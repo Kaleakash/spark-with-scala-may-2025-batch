9ba9ee26d6ff0c5364f1b3aa472e6e3d10f336d8
akash <akash300383@gmail.com>
2025-05-21 08:59:44 +0000

    done

---
+++ ScalaProjects/SparkStreamExamples/src/main/scala/streamexamples/StructuredStreamFileSystem.scala
@@ -0,0 +1,36 @@
+package streamexamples
+
+
+import org.apache.spark.sql.{Encoders, SparkSession,Seconds}
+import org.apache.spark.sql.functions._
+import org.apache.spark.streaming.Seconds
+import org.threeten.extra.Seconds
+case class Product(product:String, amount:Long)
+
+object StructuredStreamFileSystem {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[*]").
+      appName("Structured File system Streaming").getOrCreate();
+
+    import spark.implicits._
+// rules for schema to match pname and price
+    val saleSchema = Encoders.product[Product].schema
+    val salesStream  = spark.readStream.
+      option("header","true").
+      schema(saleSchema).
+      csv("data")
+
+    val saleDS = salesStream.as[Product];
+
+    // transformation
+    val totalSales = saleDS.groupBy($"product").agg(sum($"amount").as("total_amount"));
+
+    val query = totalSales.writeStream.outputMode("complete").format("console").trigger(Seconds("5 seconds")).
+      start()
+
+    query.awaitTermination();
+
+
+
+  }
+}
